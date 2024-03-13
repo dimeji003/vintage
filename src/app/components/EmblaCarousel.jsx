2edc9cee2244fc64093b0592433f8f8eba39da84
dimeji003 <oladimejilawal14@gmail.com>
2024-03-13 23:24:48 +0100

    edit files

--- src/app/components/EmblaCarousel.jsx
+++ src/app/components/EmblaCarousel.jsx
@@ -16,7 +16,7 @@ export const EmblaCarousel = () => {
         <em className='lg:w-[30%] text-white lg:text-xl text-sm text-center lg:text-left px-5 lg:px-0'>Celebrating Style, Embracing Elegance: Where Fashion Becomes a Statement.</em>
         <div className='text-white lg:text-xl text-xs text-center lg:p-4  font-bold rounded-lg m-auto lg:m-0 bg-red-700 w-[30%] p-3 lg:w-[13%] mt-6'>SHOP NOW</div>
           </div> </div>
-        <div className= {`embla__slide ${styles.slide2} px-3 flex place-content-center`}> <em className='text-white text-xl pt-[400px]'>Beyond Fashion, we're Archiitects of Expression.Builidng confidence, one outfit at a time.</em></div>
+        <div className= {`embla__slide ${styles.slide2} px-3 flex place-content-center`}> <em className='text-white text-xl pt-[400px]'>Beyond Fashion, we&apos;re Archiitects of Expression. Builidng confidence, one outfit at a time.</em></div>
         <div className= {`embla__slide ${styles.slide3}`}>Slide 3</div>
       </div>
     </div>
